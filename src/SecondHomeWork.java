6128f63a1a351558778c1a9766da5761f9b7fec3
Dmitry <f_o_r_w_a_r_d@inbox.ru>
2020-05-19 13:00:46 +0500

    Initial commit

---
+++ src/SecondHomeWork.java
@@ -0,0 +1,8 @@
+public class SecondHomeWork {
+    public static final String ANSI_RESET="\u001B[0m";
+    public static final String ANSI_GREEN="\u001B[32m";
+
+    public static void main(String[] args) {
+
+    }
+}
